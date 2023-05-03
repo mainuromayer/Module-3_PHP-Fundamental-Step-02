643764a9d1dd8de2dac4c1e6eb5b0224cdbd8bf8
mainuromayer <mainuromayer@gmail.com>
2023-05-03 12:40:01 +0600

    Default value setf function parameter |r optional parameter set in function

--- php09/function.php
+++ php09/function.php
@@ -1,8 +1,10 @@
 <?php
+
 /* 
 মনে করি একটা রোবট আছে, তাকে কমান্ড করে বলে দিতে হবে যে কি ধরনের খাবার serve করবে - এটা তাকে বলে দিব
 এবং default value সেট করে দিব
 */
+
 function serve($foodType="Coffee", $numberOfItem="1 cups"){
     echo "{$numberOfItem} of {$foodType} has/have been served";
 }
